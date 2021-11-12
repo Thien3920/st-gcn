a4b906675b71331eedd89261ef67f1d77cf8dae8
Thien3920 <87056480+Thien3920@users.noreply.github.com>
2021-11-12 15:59:59 +0700

    Update st_gcn_twostream.py

--- net/st_gcn_twostream.py
+++ net/st_gcn_twostream.py
@@ -8,19 +8,42 @@ from net.utils.graph import Graph
 
 from .st_gcn import Model as ST_GCN
 
+
+
 class Model(nn.Module):
-
-    def __init__(self, *args, **kwargs):
+    """Two inputs spatial temporal graph convolutional networks.
+    Args:
+        - graph_args: (dict) Args map of `Actionsrecognition.Utils.Graph` Class.
+        - num_class: (int) Number of class outputs.
+        - edge_importance_weighting: (bool) If `True`, adds a learnable importance
+            weighting to the edges of the graph.
+        - **kwargs: (optional) Other parameters for graph convolution units.
+    Shape:
+        - Input: :tuple of math:`((N, 3, T, V), (N, 2, T, V))`
+        for points and motions stream where.
+            :math:`N` is a batch size,
+            :math:`in_channels` is data channels (3 is (x, y, score)), (2 is (mot_x, mot_y))
+            :math:`T` is a length of input sequence,
+            :math:`V` is the number of graph nodes,
+        - Output: :math:`(N, num_class)`
+    """
+    def __init__(self, graph_args, num_class, edge_importance_weighting=True,
+                 **kwargs):
         super().__init__()
+        self.pts_stream = ST_GCN(3, graph_args, None,
+                                                     edge_importance_weighting,
+                                                     **kwargs)
+        self.mot_stream = ST_GCN(2, graph_args, None,
+                                                     edge_importance_weighting,
+                                                     **kwargs)
 
-        self.origin_stream = ST_GCN(*args, **kwargs)
-        self.motion_stream = ST_GCN(*args, **kwargs)
+        self.fcn = nn.Linear(256 * 2, num_class)
 
-    def forward(self, x):
-        N, C, T, V, M = x.size()
-        m = torch.cat((torch.cuda.FloatTensor(N, C, 1, V, M).zero_(),
-                        x[:, :, 1:-1] - 0.5 * x[:, :, 2:] - 0.5 * x[:, :, :-2],
-                        torch.cuda.FloatTensor(N, C, 1, V, M).zero_()), 2)
+    def forward(self, inputs):
+        out1 = self.pts_stream(inputs[0])
+        out2 = self.mot_stream(inputs[1])
 
-        res = self.origin_stream(x) + self.motion_stream(m)
-        return res
+        concat = torch.cat([out1, out2], dim=-1)
+        out = self.fcn(concat)
+
+        return torch.sigmoid(out)
